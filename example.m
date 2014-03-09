a83a1f0b60d444ecce66c79f1ca8d7490df8f010
emtiyaz <emtiyaz@gmail.com>
2014-03-09 21:19:39 +0100

    added computation of V

--- example.m
+++ example.m
@@ -1,19 +1,5 @@
-% Demo for a simple Gaussian process approximation
-% We generate synthetic classification data (yi,Xi)
-% We will assume a GP prior with zero mean and a linear Kernel
-% with a logit likelihood to generate binary data.
-% We will fit an approximate Gaussian posterior N(m,V) 
-% with a restriction that diagonal of V is 1.
-% Our goal is to find such m and V.
-% This corresponds to the KL method of Kuss and Rasmussen, 2005
-% We can write the objective function as 
-% max_m f(m) = -0.5* (m-mu)'*Omega*(m-mu) + sum_i f_i(yi,mi,1)
-%     where f_i = E(log p(yi|xi)) wrt N(xi|mi,1) of logit likelihood
-% We use LBFGS implemented in minFunc by Mark Schmidt
-%
 % Written by Emtiyaz, EPFL
 % Modified on March 8, 2014
-
 clear all;
 close all;
 
@@ -44,7 +30,9 @@ load('llp.mat');
 % optimize wrt m (see function simpleVariational.m for details)
 m0 = mu; % initial value
 v = ones(N,1); % fix v to 1
-[m, logLik] = minFunc(@simpleVariational, m0, optMinFunc, y, X, mu, inv(Sigma), v, bound);
+Omega = inv(Sigma);
+[m, logLik] = minFunc(@simpleVariational, m0, optMinFunc, y, X, mu, Omega, v, bound);
+V = inv(Omega + diag(v));
 
 % plot
 figure(1)
@@ -57,6 +45,4 @@ hold on
 plot(1./(1+exp(-m)), '*r','markersize', 10);
 ylim([-0.05 1.05]);
 ylabel('Prediction for training data');
-
-
 
